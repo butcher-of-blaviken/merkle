1f0d466758066dd0608f1d88970db1951c376e77
Butcher of Blaviken <butcher-of-blaviken@users.noreply.github.com>
2023-02-04 09:40:11 -0500

    add doc.go

---
+++ doc.go
@@ -0,0 +1,10 @@
+// package merkle contains a simple and naive implementation
+// of the Merkle tree data structure.
+//
+// Merkle trees provide a simple way to verify the integrity
+// of a large amount of data by way of "Merkle proofs" - a sequence
+// of carefully selected hashes, one for each level of the tree.
+//
+// Merkle trees can be n-ary trees, but here we implement binary Merkle
+// trees, since they give the smallest sized proofs.
+package merkle
